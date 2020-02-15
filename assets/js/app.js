6c9e323d8acff6f0534969f17b317bec3684b4fe
AME <adrien.mechin@gmail.com>
2020-02-15 14:38:26 +0100

    début ébauche synthese - voir minwidth

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,7 +10,7 @@ imagesContext.keys().forEach(imagesContext);
 // any CSS you import will output into a single css file (app.css in this case)
 require('../scss/app.scss');
 require('../scss/contact.scss');
-// require('../scss/flash.scss');
+require('../scss/synthese.scss');
 require('../scss/index.scss');
 require('../scss/boutique.scss');
 require('../scss/arbre.scss');
