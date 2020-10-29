e84063c176c93842ddb7dcd0abcb2cd71869a137
AME <adrien.mechin@gmail.com>
2020-10-29 21:02:18 +0100

    Revert "Delete SauvExercices.php"
    This reverts commit b3a96c7e

---
+++ src/Service/SauvExercices.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\Service;
+
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+class SauvExercices
+{
+/*    public static function SauvExos(SessionInterface $session, array $tab)
+    {
+        $session->set('idSession', $session->getId());
+        $session->set('tab', $tab);
+
+        return $session;
+    }*/
+}
