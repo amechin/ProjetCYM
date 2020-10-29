e2a5a08ea98f7b97a53b3b95f638b1ea557af4ba
AME <adrien.mechin@gmail.com>
2020-10-29 21:02:40 +0100

    Revert "Delete UserFixtures.php"
    This reverts commit 8e6bc07b

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setEmail('claire.rosart@labocollectif.fr');
+        $user->setRoles(['ROLE_ADMIN']);
+        $user->setPassword($this->passwordEncoder->encodePassword($user, 'ClCook67!'));
+        $manager->persist($user);
+
+        $user = new User();
+        $user->setEmail('adrien.mechin@gmail.com');
+        $user->setRoles(['ROLE_ADMIN']);
+        $user->setPassword($this->passwordEncoder->encodePassword($user, 'Adrien86'));
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
